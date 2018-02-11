0506d6a81fcdd2f0c8a19e1b44096c622cb28490
Kakurady <kakurady@gmail.com>
2018-02-11 10:20:48 -0500

    join the contents of two directories by hash

--- diskwasher.js
+++ diskwasher.js
@@ -4,31 +4,62 @@ const hasha = require("hasha");
 
 async function doDirectory(path){
     // list files
+    // TODO: want a sum of fiile size here. 
+    // TODO: I want a progress indicator
     let files = await recursive_readdir(path);
 
     let hashes = [];
-    for (const x of files){
+    for (const kv of files.entries()){
+        let i = kv[0];
+        let x = kv[1];
         hashes.push({
             'filename': x,
             'sha512': await hasha.fromFile(x, {algorithm:"sha512"})
          });
     }
 
-//    // make hashes
-//    // this will issue three hundred files
-//    let promises = files.map(function(x){
-
-//    });
-//    await Promise.all(promises);
-
-//    for (const x of promises){
-//        hashes.push(await x);
-//    }
     return hashes;
 }
 
-function main(){
-    let p = doDirectory(process.argv[2]);
-    p.then(x => console.log(x));
+function* join(left, ...right){
+    let key = "sha512";
+    function* dictGenerator (left){
+        for (const x of left){
+            yield [x[key], x];
+        }
+    }
+
+    let dict = new Map(dictGenerator(left));
+    
+    //FIXME consolidate more than one right
+    for (const list of right){
+        for (const y of list){
+            let x = dict.get(y[key]);
+            if (x) {
+             yield x;
+            }
+        }
+    }
+}
+
+function* filter(iter, fn){
+    for (const x of iter){
+        if (fn(x)){
+            yield x;
+        }
+    }
+}
+
+async function main(){
+    let directories = process.argv.slice(2);
+    let hashes = [];
+    for (const x of directories){
+        let hash = await doDirectory(x);
+        console.log(hash);
+        hashes.push(hash);
+    }
+
+    let joined = [... join(...hashes)];
+    console.log(joined);    
 }
 main();
